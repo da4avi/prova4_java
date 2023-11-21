367049d996447497b402050e52bf4a37303e039a
Davi <finkler.davi41@gmail.com>
2023-11-20 22:01:54 -0300

    prova4v1

---
+++ Gerenciar.java
@@ -0,0 +1,119 @@
+import java.util.Scanner;
+
+public class Gerenciar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int op = 0;
+
+        do {
+            System.out.println("Minha Biblioteca");
+            System.out.println("0 - Sair");
+            System.out.println("1 - Criar autor");
+            System.out.println("2 - Criar livro");
+            System.out.println("3 - Criar mídia digital");
+            System.out.println("4 - Criar biblioteca");
+            System.out.println("5 - Adicionar livro");
+            System.out.println("6 - Emprestar livro");
+            System.out.println("7 - Devolver livro");
+            System.out.println("8 - Adicionar mídia digital");
+            System.out.println("9 - Emprestar mídia digital");
+            System.out.println("10 - Devolver mídia digital");
+            try {
+                op = sc.nextInt();
+            } catch (Exception e) {
+                op = 99;
+            }
+
+            switch (op) {
+                case 0: {
+                    System.out.println("Sair....");
+                    break;
+                }
+                case 1: {
+                    System.out.println("Digite o nome do autor");
+                    String nome = sc.next();
+                    System.out.println("Digite o nacionalidade do autor");
+                    String nacionalidade = sc.next();
+                    new Autor(nome, nacionalidade);
+                    break;
+                }
+                case 2: {
+                    System.out.println("Digite o titulo");
+                    String titulo = sc.next();
+                    System.out.println("Digie o ID do autor");
+                    int idAutor = sc.nextInt();
+                    new Livro(titulo, null, false, idAutor);
+                    break;
+                }
+                case 3: {
+                    System.out.println("Digite o titulo");
+                    String titulo = sc.next();
+                    System.out.println("Digite o álbum");
+                    String album = sc.next();
+                    new MidiaDigital(titulo, album, false);
+                    break;
+                }
+                case 4: {
+                    System.out.println("Digite o nome");
+                    String nome = sc.next();
+                    new Biblioteca(nome);
+                    break; 
+                }
+                case 5: {
+                    System.out.println("Digite o ID da biblioteca");
+                    Biblioteca.listarBibliotecas();
+                    int idBiblioteca = sc.nextInt();
+                    System.out.println("Selecione o ID do livro");
+                    Livro.listarLivros();
+                    int idLivro = sc.nextInt();
+                    Biblioteca.adicionarLivro(idBiblioteca, idLivro);
+                    break;
+                }
+                case 6: {
+                    System.out.println("Selecione o ID do livro que você deseja emprestar");
+                    Livro.listarLivros();
+                    int idLivro = sc.nextInt();
+                    Livro.emprestarLivro(idLivro);
+                    break;
+                }
+                case 7: {
+                    System.out.println("Selecione o ID do livro que você deseja devolver");
+                    Livro.listarLivros();
+                    int idLivro = sc.nextInt();
+                    Livro.devolverLivro(idLivro);
+                    break;
+                }
+                case 8: {
+                    System.out.println("Selecione o ID da biblioteca");
+                    Biblioteca.listarBibliotecas();
+                    int idBiblioteca = sc.nextInt();
+                    System.out.println("Selecione o ID da mídia digital");
+                    MidiaDigital.listarMidiaDigital();
+                    int idMidiaDigital = sc.nextInt();
+                    Biblioteca.adicionarMidiaDigital(idBiblioteca, idMidiaDigital);
+                    break;
+                }
+                case 9: {
+                    System.out.println("Selecione o ID da mídia digital que você deseja emprestar");
+                    MidiaDigital.listarMidiaDigital();
+                    int idMidiaDigital = sc.nextInt();
+                    MidiaDigital.emprestarMidiaDigital(idMidiaDigital);
+                    break;
+                }
+                case 10: {
+                    System.out.println("Selecione o ID da mídia digital que você deseja devolver");
+                    MidiaDigital.listarMidiaDigital();
+                    int idMidiaDigital = sc.nextInt();
+                    MidiaDigital.devolverMidiaDigital(idMidiaDigital);
+                    break;
+                }
+                default: {
+                    System.out.println("Opção inválida");
+                    break;
+                }
+            }
+        } while (op != 0);
+        sc.close();
+    }
+}
---
+++ Livro.java
@@ -0,0 +1,129 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Livro extends Midia {
+    private Autor autor;
+    public int idAutor;
+
+    public Livro(String titulo, Autor autor, boolean disponivel, int idAutor) {
+        super(titulo, disponivel);
+        this.autor = autor;
+        try {
+            Connection connManager = DriverManager
+                    .getConnection(
+                            "jdbc:mysql://localhost:3306/biblioteca",
+                            "root",
+                            "");
+            System.out.println("Conexão estabelecida!");
+            PreparedStatement ps = connManager.prepareStatement("insert into biblioteca.livro values (?, ?, ?, ?)");
+            ps.setLong(1, 1);
+            ps.setString(2, titulo);
+            ps.setBoolean(3, disponivel);
+            ps.setInt(4, Autor.getAutorId(idAutor));
+            ps.executeUpdate();
+            connManager.close();
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    public String toString() {
+        return "Título: " + super.getTitulo()
+            + ". Autor: " + this.autor.getNome() 
+            + ". Disponível: " + (super.getDisponivel() ? "Sim" : "Não");
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public Autor getAutor() {
+        return this.autor;
+    }
+
+    public static void emprestarLivro(int idLivro) {
+        try {
+            Connection connManager = DriverManager
+                    .getConnection(
+                            "jdbc:mysql://localhost:3306/biblioteca",
+                            "root",
+                            "");
+            System.out.println("Conexão estabelecida!");
+            PreparedStatement ps = connManager.prepareStatement("update biblioteca.livro set disponivel = false where id_livro = ?");
+            ps.setInt(1, Livro.getLivroId(idLivro));
+            ps.executeUpdate();
+            connManager.close();
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    public static void devolverLivro(int idLivro) {
+        try {
+            Connection connManager = DriverManager
+                    .getConnection(
+                            "jdbc:mysql://localhost:3306/biblioteca",
+                            "root",
+                            "");
+            System.out.println("Conexão estabelecida!");
+            PreparedStatement ps = connManager.prepareStatement("update biblioteca.livro set disponivel = true where id_livro = ?");
+            ps.setInt(1, Livro.getLivroId(idLivro));
+            ps.executeUpdate();
+            connManager.close();
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    public static int getLivroId(int idLivro) {
+        try {
+            Connection connManager = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/biblioteca",
+                    "root",
+                    "");
+            System.out.println("Conexão estabelecida");
+ 
+            String query = "SELECT id_livro FROM biblioteca.livro WHERE id_livro = ?";
+            PreparedStatement ps = connManager.prepareStatement(query);
+            ps.setInt(1, idLivro);
+            ResultSet rs = ps.executeQuery();
+ 
+            if (rs.next()) {
+                 idLivro = rs.getInt("id_livro");
+            } else {
+                System.out.println("Livro não encontrado");
+            }
+            rs.close();
+            connManager.close();
+        } catch (SQLException erro) {
+            System.out.println(erro.getMessage());
+        }
+        return idLivro;
+    }
+
+    public static void listarLivros () {
+        try {
+            Connection connManager = DriverManager
+                    .getConnection(
+                            "jdbc:mysql://localhost:3306/biblioteca",
+                            "root",
+                            "");
+            System.out.println("Conexão estabelecida!");
+            PreparedStatement ps = connManager.prepareStatement("select id_livro from biblioteca.livro");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                int idLivro = rs.getInt("id_livro");
+                System.out.println("ID do Livro: " + idLivro);
+            }
+            rs.close();
+            ps.close();
+            connManager.close();
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+}
